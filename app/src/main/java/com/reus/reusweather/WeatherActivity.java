d4cdeacb1a3d71357ab5f6e0e4746748dd908f55
Reus <wjxxwh@126.com>
2017-07-31 19:16:47 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/reus/reusweather/WeatherActivity.java
+++ app/src/main/java/com/reus/reusweather/WeatherActivity.java
@@ -1,17 +1,20 @@
 package com.reus.reusweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.reus.reusweather.gson.Forecast;
 import com.reus.reusweather.gson.Weather;
 import com.reus.reusweather.util.HttpUtil;
@@ -23,41 +26,8 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-//public class WeatherActivity extends AppCompatActivity {
-//
-//    private ScrollView weatherLayout;
-//
-//    private TextView titleCity;
-//
-//    private TextView titleUpdateTime;
-//
-//    //下面两个是now部分，显示当前气温和天气状况
-//    private TextView degreeText;
-//
-//    private TextView weatherInfoText;
-//
-//    private LinearLayout forecastLayout;                                        //未来天气预报的布局
-//
-//    private TextView aqiText;                                                   //显示AQI指数
-//
-//    private TextView pm25Text;                                                  //显示PM2.5指数
-//
-//    //下面三个是suggestion部分
-//    private TextView comfortText;                                               //
-//
-//    private TextView carWashText;
-//
-//    private TextView sportText;
-//
-
-//
-//
-
-//
-//}
 public class WeatherActivity extends AppCompatActivity {
 
-    public DrawerLayout drawerLayout;
 
     private ScrollView weatherLayout;
 
@@ -81,11 +51,21 @@ public class WeatherActivity extends AppCompatActivity {
 
     private TextView sportText;
 
+    private ImageView bingPicImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         //初始化各控件
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
         titleCity = (TextView) findViewById(R.id.title_city);
         titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
@@ -108,6 +88,13 @@ public class WeatherActivity extends AppCompatActivity {
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);                                //将ScrollView进行隐藏
             requestWeather(weatherId);
+        }
+        //bingPic是图片的链接
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        } else {
+            loadBingPic();
         }
 
     }
@@ -155,6 +142,35 @@ public class WeatherActivity extends AppCompatActivity {
             }
 
         });
+        loadBingPic();
+    }
+
+    /**
+     * 加载必应每日一图
+     */
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this)
+                        .edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+        });
     }
 
 
@@ -190,7 +206,7 @@ public class WeatherActivity extends AppCompatActivity {
         }
         String comfort = "舒适度：" + weather.suggestion.comfort.info;
         String carWash = "洗车指数：" + weather.suggestion.carWash.info;
-        String sport = "运行建议：" + weather.suggestion.sport.info;
+        String sport = "运动建议：" + weather.suggestion.sport.info;
         comfortText.setText(comfort);
         carWashText.setText(carWash);
         sportText.setText(sport);
